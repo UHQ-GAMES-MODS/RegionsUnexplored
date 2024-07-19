f4cf42d0b6b60e4fcc125fe80cafe2ea2ffa4d97
KirboSoftware <joostmsoftware@gmail.com>
2024-07-19 07:22:02 +0200

    In this commit: - Fixed missing autoservice

--- fabric/src/main/java/net/regions_unexplored/platform/FabricParticleRegistry.java
+++ fabric/src/main/java/net/regions_unexplored/platform/FabricParticleRegistry.java
@@ -1,11 +1,13 @@
 package net.regions_unexplored.platform;
 
+import com.google.auto.service.AutoService;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
 import net.minecraft.client.particle.ParticleEngine;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleType;
 import net.regions_unexplored.platform.services.IParticleRegistry;
 
+@AutoService(IParticleRegistry.class)
 public class FabricParticleRegistry implements IParticleRegistry {
     @Override
     public <T extends ParticleOptions> void register(ParticleType<T> type, ParticleEngine.SpriteParticleRegistration<T> factory) {
