b72dabc688260b38d0fedeaf934d08a38b9577b6
Corgi Taco <66983020+CorgiTaco@users.noreply.github.com>
2024-07-21 19:27:23 -0700

    -Fix Fabric colors

--- fabric/src/main/java/net/regions_unexplored/client/RegionsUnexploredFabricClient.java
+++ fabric/src/main/java/net/regions_unexplored/client/RegionsUnexploredFabricClient.java
@@ -2,11 +2,14 @@ package net.regions_unexplored.client;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.regions_unexplored.RegionsUnexploredFabric;
+import net.regions_unexplored.client.color.RuColors;
 
 public class RegionsUnexploredFabricClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         RegionsUnexploredClient.clientInit();
+        RuColors.tintBlocks();
+        RuColors.tintItems();
         RegionsUnexploredFabric.afterRegistriesFreeze();
     }
 }
