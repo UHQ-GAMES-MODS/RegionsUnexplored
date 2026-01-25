d25503a93ef189c89246a46746691483cb135ef5
Apollo <102649729+Apollounknowndev@users.noreply.github.com>
2026-01-25 14:58:30 -0500

    Move creative mode tab, add vanilla tab injection

--- src/neoforge/main/java/net/regions_unexplored/RegionsUnexploredNeoClient.java
+++ src/neoforge/main/java/net/regions_unexplored/RegionsUnexploredNeoClient.java
@@ -1,11 +1,17 @@
 package net.regions_unexplored;
 
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.level.ItemLike;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.regions_unexplored.client.ParticleRegistration;
 import net.regions_unexplored.client.TintRegistration;
+import net.regions_unexplored.registry.RUCreativeModeTabs;
+
+import java.util.function.BiConsumer;
 
 @Mod(value = RegionsUnexplored.MOD_ID, dist = Dist.CLIENT)
 public class RegionsUnexploredNeoClient {
@@ -14,5 +20,29 @@ public class RegionsUnexploredNeoClient {
         bus.addListener(TintRegistration::registerBlockColorHandlers);
         bus.addListener(TintRegistration::registerItemColorHandlers);
         bus.addListener(ParticleRegistration::registerParticleProviders);
+        bus.addListener(RegionsUnexploredNeoClient::addToVanillaCreativeModeTabs);
+    }
+
+    private static void addToVanillaCreativeModeTabs(BuildCreativeModeTabContentsEvent event) {
+        var consumer = getVanillaCreativeModeTabAdder(event);
+        if (event.getTabKey().equals(CreativeModeTabs.BUILDING_BLOCKS)) {
+            RUCreativeModeTabs.addToBuildingBlocks(consumer);
+        } else if (event.getTabKey().equals(CreativeModeTabs.COLORED_BLOCKS)) {
+            RUCreativeModeTabs.addToColoredBlocks(consumer);
+        } else if (event.getTabKey().equals(CreativeModeTabs.FUNCTIONAL_BLOCKS)) {
+            RUCreativeModeTabs.addToFunctionalBlocks(consumer);
+        } else if (event.getTabKey().equals(CreativeModeTabs.TOOLS_AND_UTILITIES)) {
+            RUCreativeModeTabs.addToToolsAndUtilities(consumer);
+        } else if (event.getTabKey().equals(CreativeModeTabs.FOOD_AND_DRINKS)) {
+            RUCreativeModeTabs.addToFoodAndDrinks(consumer);
+        }
+    }
+
+    private static BiConsumer<ItemLike, ItemLike> getVanillaCreativeModeTabAdder(BuildCreativeModeTabContentsEvent event) {
+        return (a, b) -> event.insertAfter(
+            a.asItem().getDefaultInstance(),
+            b.asItem().getDefaultInstance(),
+            CreativeModeTab.TabVisibility.PARENT_TAB_ONLY
+        );
     }
 }
